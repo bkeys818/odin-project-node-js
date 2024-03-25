bb163e6aa2e546aacfc0aa2516e3ee91f0de04bc
Ben Keys <bkeys818@gmail.com>
2024-03-25 10:18:25 -0400

    💄 Create homepage

--- local-library/controllers/bookController.ts
+++ local-library/controllers/bookController.ts
@@ -1,8 +1,32 @@
 import asyncHandler from 'express-async-handler'
 import Book from '../models/book'
+import BookInstance from '../models/bookinstance'
+import Author from '../models/author'
+import Genre from '../models/genre'
 
 export const index = asyncHandler(async (req, res, next) => {
-    res.send('NOT IMPLEMENTED: Site Home Page')
+    const [
+        numBooks,
+        numBookInstances,
+        numAvailableBookInstances,
+        numAuthors,
+        numGenres,
+    ] = await Promise.all([
+        Book.countDocuments({}).exec(),
+        BookInstance.countDocuments({}).exec(),
+        BookInstance.countDocuments({ status: 'Available' }).exec(),
+        Author.countDocuments({}).exec(),
+        Genre.countDocuments({}).exec(),
+    ])
+
+    res.render('index', {
+        title: 'Local Library Home',
+        book_count: numBooks,
+        book_instance_count: numBookInstances,
+        book_instance_available_count: numAvailableBookInstances,
+        author_count: numAuthors,
+        genre_count: numGenres,
+    })
 })
 
 // Display list of all books.
