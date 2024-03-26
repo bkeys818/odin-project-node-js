86cc4ebceb41cbae88250cc497167e2de03113e3
Ben Keys <bkeys818@gmail.com>
2024-03-26 08:53:20 -0400

    💄 Create genre detail

--- local-library/controllers/genreController.ts
+++ local-library/controllers/genreController.ts
@@ -1,5 +1,7 @@
 import asyncHandler from 'express-async-handler'
 import Genre from '../models/genre'
+import Book from '../models/book'
+import StatusError from '../utils/statusError'
 
 // Display list of all Genre.
 export const genreList = asyncHandler(async (req, res, next) => {
@@ -12,7 +14,21 @@ export const genreList = asyncHandler(async (req, res, next) => {
 
 // Display detail page for a specific Genre.
 export const genreDetail = asyncHandler(async (req, res, next) => {
-    res.send(`NOT IMPLEMENTED: Genre detail: ${req.params.id}`)
+    const [genre, booksInGenre] = await Promise.all([
+        Genre.findById(req.params.id).exec(),
+        Book.find({ genre: req.params.id }, 'title summary').exec(),
+    ])
+    if (genre === null) {
+        // No results.
+        const err = new StatusError('Genre not found')
+        err.status = 404
+        return next(err)
+    }
+    res.render('genre-detail', {
+        title: 'Genre Detail',
+        genre: genre,
+        genre_books: booksInGenre,
+    })
 })
 
 // Display Genre create form on GET.
